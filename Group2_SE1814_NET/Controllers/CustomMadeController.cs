696f2120cd18e5dce25afedb18c4f4e7d6e43f85
vietsonvtpt <vietsonvtpt@gmail.com>
2025-06-03 00:26:35 +0700

    add some ui

---
+++ Group2_SE1814_NET/Controllers/CustomMadeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group2_SE1814_NET.Controllers {
+    public class CustomMadeController : Controller {
+        // GET: CustomMadeController
+        public ActionResult Index() {
+            return View();
+        }
+
+    }
+}
